88e37e7bf3ab00f0a573217c0d07153bac639749
Cleanly <oskar.turesson99@gmail.com>
2020-03-30 16:58:17 +0200

    added mobile styling for footer

--- public/themes/gatenhielmska/footer.php
+++ public/themes/gatenhielmska/footer.php
@@ -11,10 +11,12 @@
             <li class="footer_list_item"><a href="">Tillgänglighet</a></li>
             <li class="footer_list_item"><a href="">Om Gatenhielmska</a></li>
         </ul>
-        <p class="footer_copy"><span class="hideOnMobile">Stigbergstorget 7, 414 63 Göteborg</span> <br>
-            © 2020 Gathenhielmska Huset </p>
+        <div class="hideOnMobile">
+            <p class="footer_copy">Stigbergstorget 7, 414 63 Göteborg <br>
+                © 2020 Gathenhielmska Huset </p>
+        </div>
     </div>
-    <div class="footer_parts">
+    <div class="footer_parts hideOnMobile">
         <ul class="footer_social">
             <a class="footer_social_item" href=""><img class="footer_social_item_images" src="/themes/gatenhielmska/assets/images/facebook.svg" alt="facebook"></a>
             <a class="footer_social_item" href=""><img class="footer_social_item_images" src="/themes/gatenhielmska/assets/images/instagram.svg" alt="instagram"></a>
@@ -23,9 +25,18 @@
         <ul class="footer_social"><a class="footer_info" href="">Om Webbplatsen</a> / <a class="footer_info" href="">Personuppgifter</a> / <a class="footer_info" href="">Allmänna Villkor</a></ul>
     </div>
     <div class="footer_parts">
-
+        <ul class="footer_social hideOnDesktop">
+            <a class="footer_social_item" href="hej.se">Instagram</a>
+            <a class="footer_social_item" href="">Facebook</a>
+            <a class="footer_social_item" href="">Twitter</a>
+        </ul>
         <img class="footer_flower" src="/themes/gatenhielmska/assets/images/blomma.png" alt="">
+
     </div>
+
+    <p class="footer_copy hideOnDesktop">© 2020 Gathenhielmska Huset </p>
+
+    <img class="footer_wave hideOnDesktop" src="/themes/gatenhielmska/assets/images/waveFooter.svg" alt="">
 
 </div>
 <?php wp_footer(); ?>
--- public/themes/gatenhielmska/page-templates/event.php
+++ public/themes/gatenhielmska/page-templates/event.php
@@ -10,15 +10,17 @@
 <?php $events = get_posts(['post_type' => 'event']); ?>
 <div class="preview_container">
     <?php foreach ($events as $post) : setup_postdata($post) ?>
-        <?php if (get_field('event_start') >= date('F j, Y')) : ?>
-            <a href="<?php the_permalink() ?>" class="preview_event">
-                <img class="preview_image" src="<?php the_field('event_image') ?>" alt="Event image">
-                <div class="preview_overlay">
-                    <p class="preview_title"><?php the_field('event_name') ?></p>
-                    <p class="preview_date"><?php the_field('event_start') ?> - <?php the_field('event_end') ?></p>
-                </div>
-            </a>
-        <?php endif; ?>
+        <?php // if (get_field('event_start') >= date('F j, Y')) :
+        ?>
+        <a href="<?php the_permalink() ?>" class="preview_event">
+            <img class="preview_image" src="<?php the_field('event_image') ?>" alt="Event image">
+            <div class="preview_overlay">
+                <p class="preview_title"><?php the_field('event_name') ?></p>
+                <p class="preview_date"><?php the_field('event_start') ?> - <?php the_field('event_end') ?></p>
+            </div>
+        </a>
+        <?php //endif;
+        ?>
     <?php endforeach; ?>
 </div>
 
