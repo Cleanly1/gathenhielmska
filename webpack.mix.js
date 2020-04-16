e201f2c39666707263c38d5d8df8cc5ec9405381
Cleanly <oskar.turesson99@gmail.com>
2020-04-17 00:05:14 +0200

    final touches

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,7 +21,7 @@ mix.setPublicPath(`public/themes/${theme}/assets`);
 // mix.js("resources/scripts/app.js", "scripts");
 mix
   .sass("resources/styles/app.scss", "styles")
-  .sass("resources/styles/components/event.scss", "styles")
+  // .sass("resources/styles/components/event.scss", "styles")
   // .sass("resources/styles/front-page.scss", "styles")
   // .sass("resources/styles/components/footer.scss", "styles")
   .options({
