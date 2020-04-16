0378110a8c0eea2bd3174f035f21c392aba4ca44
Jakob Clausen <jakob.fridmar@gmail.com>
2020-04-16 08:03:20 +0200

    Added more sections to gallery

--- public/themes/gatenhielmska/gallery-garden.php
+++ public/themes/gatenhielmska/gallery-garden.php
@@ -8,6 +8,19 @@
         <div></div>
     </div>
 
+
+    <ul class="gallery-desktop-sub-nav">
+                <?php foreach (get_pages(['sort_column' => 'menu_order', 'sort_order' => 'desc', 'parent' => 14]) as $childPage) { ?>
+                    <li class="gallery-sub-nav-list">
+                        <a href="<?php echo get_page_link($childPage->ID); ?>"><?php echo $childPage->post_title; ?></a>
+                    </li>
+                <?php } ?>
+        </ul>
+
+        <div class="gallery-separator">
+            <p>Bilder från Gathenhielmska Huset</p>
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
